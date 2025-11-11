c9d976f1f17bc2ab4afbe05e64d86c270a75351e
ELBEBACK <elbekbegaliev305@gmail.com>
2025-11-12 02:52:38 +0300

    3 workers semaphore task

---
+++ 3 workers at class/3workers.c
@@ -0,0 +1,131 @@
+#include "safe-semaphors.h"
+#include <sys/wait.h>
+#include <stdbool.h>
+
+void create_workers(int semid);
+void wrench_worker(int semid, int num);
+void screwdriver_worker(int semid, int num);
+void replace_device(int semid);
+
+int devices = 1;
+
+// sem_array = | A = 2 | B = 1 | MUTEX = 1 | Ready = 0 |
+int main(void) {
+    
+    int semid = create_semaphore("main.c", IPC_CREAT|0644);
+  
+    create_workers(semid);
+
+    for (size_t i = 0; i < 3; i++) wait(NULL);
+
+    printf("All processes are ended\n");
+    delete_semaphore(semid);
+    return 0;
+}
+
+void create_workers(int semid) {
+    
+    for (int i = 1; i <= 2; i++) {
+        pid_t pid = fork_safe();
+        if (pid == 0) {
+        wrench_worker(semid, i);
+        exit(EXIT_SUCCESS);
+        }
+    }
+  
+    pid_t pid = fork_safe();
+    if (pid == 0) {
+        screwdriver_worker(semid, 1);
+        exit(EXIT_SUCCESS);
+    }
+}
+
+void wrench_worker(int semid, int num) {
+    
+    struct sembuf P_A[] = {{0, -1, 0}};        // P(A)
+    struct sembuf P_mutex[] = {{2, -1, 0}};    // P(MUTEX)
+    struct sembuf V_mutex[] = {{2, 1, 0}};     // V(MUTEX)
+    struct sembuf V_A[] = {{0, 1, 0}};         // V(A)
+    struct sembuf V_B[] = {{1, 1, 0}};         // V(B)
+  
+    int details_installed = 0;
+  
+    while (details_installed < devices) {
+        
+        RunOp_safe(semid, P_A, 1);               // P(A)
+    
+
+        RunOp_safe(semid, P_mutex, 1);           // P(MUTEX)
+
+
+        printf("Wrench worker %d: installing nut\n", num);
+        sleep(1); 
+    
+        
+        int current_ready = GetVal_safe(semid, 3);
+        SetVal_safe(semid, 3, current_ready + 1);
+    
+
+        if (current_ready + 1 == 3) {
+            printf("Wrench worker %d: device completed\n", num);
+            replace_device(semid);
+        
+
+        RunOp_safe(semid, V_A, 1);             // V(A)
+        RunOp_safe(semid, V_A, 1);             // V(A)
+        RunOp_safe(semid, V_B, 1);             // V(B)
+        }
+    
+        RunOp_safe(semid, V_mutex, 1);           // V(MUTEX)
+        details_installed++;
+    }
+  
+    printf("Wrench worker %d finished\n", num);
+}
+
+void screwdriver_worker(int semid, int num) {
+    struct sembuf P_B[] = {{1, -1, 0}};        // P(B)
+    struct sembuf P_mutex[] = {{2, -1, 0}};    // P(MUTEX)
+    struct sembuf V_mutex[] = {{2, 1, 0}};     // V(MUTEX)
+    struct sembuf V_A[] = {{0, 1, 0}};         // V(A)
+    struct sembuf V_B[] = {{1, 1, 0}};         // V(B)
+  
+    int details_installed = 0;
+  
+        while (details_installed < devices) {
+            
+            RunOp_safe(semid, P_B, 1);               // P(B)
+    
+
+            RunOp_safe(semid, P_mutex, 1);           // P(MUTEX)
+            
+            
+            printf("Screwdriver worker %d: installing screw\n", num);
+            sleep(1); 
+    
+            
+            int current_ready = GetVal_safe(semid, 3);
+            SetVal_safe(semid, 3, current_ready + 1);
+    
+            
+            if (current_ready + 1 == 3) {
+                
+                printf("Screwdriver worker %d: device completed\n", num);
+                replace_device(semid);
+      
+                RunOp_safe(semid, V_A, 1);             // V(A)
+                RunOp_safe(semid, V_A, 1);             // V(A)
+                RunOp_safe(semid, V_B, 1);             // V(B)
+            }
+    
+            RunOp_safe(semid, V_mutex, 1);           // V(MUTEX)
+            details_installed++;
+    }
+  
+    printf("Screwdriver worker %d finished\n", num);
+}
+
+void replace_device(int semid) {
+    SetVal_safe(semid, 3, 0);  // Ready = 0
+    printf("-------Device replaced-------\n");
+}
---
+++ 3 workers at class/safe-semaphors.c
@@ -0,0 +1,66 @@
+#include "safe-semaphors.h"
+
+pid_t fork_safe(void) {
+    pid_t p = fork();
+    if (p < 0) {
+        perror("fork creation fail");
+        exit(EXIT_FAILURE);
+    }
+    return p;
+}
+
+
+int GetVal_safe(int semid, int num_in_sem) {
+    
+    int return_value = semctl(semid, num_in_sem, GETVAL);
+    if (return_value < 0) {
+        perror("semctl GetVal fail\n");
+        exit(EXIT_FAILURE);
+    }
+    return return_value;
+}
+
+
+void SetVal_safe(int semid, int num_in_sem, int value) {
+    
+    if (semctl(semid, num_in_sem, SETVAL, value) < 0) {
+        perror("semctl SetVal fail\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+void RunOp_safe(int semid, struct sembuf *op, size_t nop) {
+    
+    if (semop(semid, op, nop) < 0) {
+        perror("semop RunOp fail");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+int create_semaphore(const char* name, int flags) {
+
+    int semid = semget(ftok(name, 1), 4, flags);
+    if (semid == -1) {
+        perror("semaphore creation fail\n");
+        exit(EXIT_FAILURE);
+    }
+
+    SetVal_safe(semid, 0, 2);  // A = 2
+    SetVal_safe(semid, 1, 1);  // B = 1
+    SetVal_safe(semid, 2, 1);  // MUTEX = 1
+    SetVal_safe(semid, 3, 0);  // Ready = 0
+  
+    printf("Semaphore is created\n");
+    return semid;
+}
+
+void delete_semaphore(int semid) {
+    
+    if (semctl(semid, IPC_RMID, 0) < 0) {
+        perror("semaphore deletion fail\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("Semaphore is deleted!\n");
+}
